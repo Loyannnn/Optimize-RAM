5d1c6f0b71bf29d9519ec40e5dd10637172c3a94
Loyannnn <tranthiennhat4@gmail.com>
2026-07-15 22:03:31 +0700

    Improve admin privilege request method
    Updated script to request administrator privileges using PowerShell.

--- Optimize-RAM-v4-GodMode.bat
+++ Optimize-RAM-v4-GodMode.bat
@@ -10,12 +10,11 @@ echo.
 
 net session >nul 2>&1
 if %errorLevel% neq 0 (
-    echo  [!] ADMINISTRATOR REQUIRED!
+    echo  [!] Requesting administrator privileges...
+    echo      ^(Windows UAC dialog will appear -- click Yes to continue^)
     echo.
-    echo      RIGHT-CLICK this file -> Run as administrator
-    echo.
-    pause
-    exit /b 1
+    PowerShell -NoProfile -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b 0
 )
 
 echo  [i] Checking Windows version...
